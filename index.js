287cb9642d1ee21b0177cc4f59a75cba363772d6
SamYuan1990 <yy19902439@126.com>
2020-10-12 18:06:05 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function getrandom(num , mul) {
 }
 
 var TestTruns = 10
-var BatchTimeout = getrandom(TestTruns,1.5)
-var TPS = getrandom(TestTruns,600)
-var MaxMessageCount = getrandom(TestTruns,120)
-var AbsoluteMaxBytes = getrandom(TestTruns,128)
-var PreferredMaxBytes = getrandom(TestTruns,2048)
+var BatchTimeout = [0.75,0.75,0.75,0.75,1,1,1,1,2,2,2,2,1.5,1.5,1.5,1.5]//getrandom(TestTruns,1.5)
+var TPS = [180,291,333,351,172,291,337,319,182,260,323.54,323.52,172,268,348,310]//getrandom(TestTruns,600)
+var MaxMessageCount = [10,40,80,120,10,40,80,120,10,40,80,120,10,40,80,120]//getrandom(TestTruns,120)
+var AbsoluteMaxBytes = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]//getrandom(TestTruns,128)
+var PreferredMaxBytes = [256,256,256,256,256,256,256,256,256,256,256,256,256,256,256,256]//getrandom(TestTruns,2048)
 
 var Turn1 =  {
      opacity: 0.5,
@@ -27,7 +27,7 @@ var Turn1 =  {
 var Turn2 =  {
      opacity: 0.5,
      color: 'pink',
-     type: 'mesh3d',
+     type: 'scatter3d',//'mesh3d',
      x: BatchTimeout,
      y: AbsoluteMaxBytes,
      z: TPS,
@@ -37,7 +37,7 @@ var Turn2 =  {
 var Turn3 = {
      opacity:0.4,
      color:'rgb(033,255,100)',
-     type: 'mesh3d',
+     type: 'scatter3d',
      x: BatchTimeout,
      y: PreferredMaxBytes,
      z: TPS,
@@ -47,7 +47,7 @@ var Turn3 = {
 var Turn4 = {
      opacity: 0.5,
      color:'rgb(200,100,200)',
-     type: 'mesh3d',
+     type: 'scatter3d',
      x: MaxMessageCount,
      y: AbsoluteMaxBytes,
      z: TPS,
@@ -57,7 +57,7 @@ var Turn4 = {
 var Turn5 =  {
     opacity: 0.5,
     color:'rgb(00,150,200)',
-    type: 'mesh3d',
+    type: 'scatter3d',
     x: MaxMessageCount,
     y: PreferredMaxBytes,
     z: TPS,
@@ -67,7 +67,7 @@ var Turn5 =  {
 var Turn6 =  {
     opacity: 0.5,
     color:'rgb(033,150,200)',
-    type: 'mesh3d',
+    type: 'scatter3d',
     x: AbsoluteMaxBytes,
     y: PreferredMaxBytes,
     z: TPS,
